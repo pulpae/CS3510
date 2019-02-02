d115d0da24d2d800d458e388bfc8c1a15ddb85b7
Anna K <annakotrba@gmail.com>
2019-02-02 15:30:22 +0000

    01

--- test.hs
+++ test.hs
@@ -1,19 +1,20 @@
--- (1)
+-- (1) Defining simple functions:
 double x = x + x
 quadruple x = double (double x)
 factorial n = product [1..n]
 average ns = sum ns `div` length ns
 
--- (2)
+-- (2) Diving integer by the length of a list:
 listDivInt = a `div` length xs
        where
           a = 10
           xs = [1,2,3,4,5]
 
--- definitions must have the same spacing. Function wrapped in wrong symbols.
+-- consecutive definitions must have the same margin.
+-- function wrapped in wrong symbols.
 -- function name must begin by a lowercase letter.
 
--- (3)
+-- (3) Fixing sorting algorithm:
 qsort [] = []
 qsort (x:xs) =
   qsort smaller ++ [x] ++ qsort larger
@@ -26,10 +27,18 @@ qsort (x:xs) =
 -- because of the comparisons a<x and b>x. A larger or equal to operator must be
 -- included instead (a<=x).
 
--- (b)
+-- (b) Descending/reverse sorting:
 qsortRev [] = []
 qsortRev (x:xs) =
   qsortRev larger ++ [x] ++ qsortRev smaller
   where
     smaller = [a | a <- xs, a <= x]
     larger = [b | b <- xs, b > x]
+
+-- (4) Defining 'last' using other functions:
+
+selLast xs = xs !! (length xs-1)
+
+revHead xs = head b
+  where
+    b = reverse xs
